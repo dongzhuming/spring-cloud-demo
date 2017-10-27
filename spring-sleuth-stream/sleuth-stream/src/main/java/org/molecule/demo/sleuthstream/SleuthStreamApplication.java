da611809bd8bb3cbb26e2e49a6cdb09dc709991b
dongzhuming <dongzhuming@gmail.com>
2017-10-28 00:22:05 +0800

    init

---
+++ spring-sleuth-stream/sleuth-stream/src/main/java/org/molecule/demo/sleuthstream/SleuthStreamApplication.java
@@ -0,0 +1,12 @@
+package org.molecule.demo.sleuthstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleuthStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleuthStreamApplication.class, args);
+	}
+}
