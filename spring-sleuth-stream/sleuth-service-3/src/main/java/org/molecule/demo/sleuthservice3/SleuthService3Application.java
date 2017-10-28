3250a615e10a41e69bf1a52b351cbc91135d2704
dongzhuming <dongzhuming@gmail.com>
2017-10-28 14:04:02 +0800

    init

---
+++ spring-sleuth-stream/sleuth-service-3/src/main/java/org/molecule/demo/sleuthservice3/SleuthService3Application.java
@@ -0,0 +1,12 @@
+package org.molecule.demo.sleuthservice3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleuthService3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleuthService3Application.class, args);
+	}
+}
