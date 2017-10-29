c52e715af738e20ef02835ac929491e7171150d9
dongzhuming <dongzhuming@gmail.com>
2017-10-29 15:36:43 +0800

    init

--- spring-sleuth-stream/sleuth-service-3/src/main/java/org/molecule/demo/sleuthservice3/SleuthService3Application.java
+++ spring-sleuth-stream/sleuth-service-3/src/main/java/org/molecule/demo/sleuthservice3/SleuthService3Application.java
@@ -2,8 +2,16 @@ package org.molecule.demo.sleuthservice3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.stream.SleuthSink;
+import org.springframework.cloud.sleuth.stream.SleuthStreamAutoConfiguration;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.integration.annotation.MessageEndpoint;
 
-@SpringBootApplication
+@EnableZipkinStreamServer
+@EnableBinding(SleuthSink.class)
+@SpringBootApplication(exclude = SleuthStreamAutoConfiguration.class)
+@MessageEndpoint
 public class SleuthService3Application {
 
 	public static void main(String[] args) {
