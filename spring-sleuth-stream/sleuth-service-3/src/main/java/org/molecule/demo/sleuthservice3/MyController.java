2dc9a524e37698858645c1864faedecefa8317e6
dongzhuming <dongzhuming@gmail.com>
2017-10-29 17:13:25 +0800

    Sleuth demo

---
+++ spring-sleuth-stream/sleuth-service-3/src/main/java/org/molecule/demo/sleuthservice3/MyController.java
@@ -0,0 +1,21 @@
+package org.molecule.demo.sleuthservice3;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by dongz on 2017/10/29.
+ */
+@RestController
+public class MyController {
+
+    private static final Logger logger = LoggerFactory.getLogger(MyController.class);
+
+    @RequestMapping("/hi")
+    public String sayHi() {
+        logger.info("Say hi");
+        return "hi";
+    }
+}
