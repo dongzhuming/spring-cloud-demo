f804f54af746ab80a295a8ae87e293dc88d62595
dongzhuming <dongzhuming@gmail.com>
2017-12-08 14:07:45 +0800

    Stream Rabbit demo

--- spring-cloud-stream/message-sender/src/main/java/org/molecule/demo/spring/cloud/stream/messagesender/MessageSenderApplication.java
+++ spring-cloud-stream/message-sender/src/main/java/org/molecule/demo/spring/cloud/stream/messagesender/MessageSenderApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.support.MessageBuilder;
 
 
@@ -18,9 +17,14 @@ import org.springframework.messaging.support.MessageBuilder;
 @EnableBinding(Source.class)
 public class MessageSenderApplication implements ApplicationRunner {
 
-	@SendTo(Source.OUTPUT)
-	public Message<?> publish() {
-		return MessageBuilder.withPayload("A useless message").build();
+	private final Source source;
+
+	public MessageSenderApplication(Source source) {
+		this.source = source;
+	}
+
+	private void publish() {
+		source.output().send(MessageBuilder.withPayload("A useless message").build());
 	}
 
 	public static void main(String[] args) {
