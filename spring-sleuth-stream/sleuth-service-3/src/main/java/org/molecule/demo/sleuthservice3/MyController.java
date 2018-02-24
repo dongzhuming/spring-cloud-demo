9e6d3a506a9600bdf6d971fd3353d7560f28e5d9
dongzhuming <dongzhuming@gmail.com>
2018-02-24 09:40:09 +0800

    Stream Rabbit demo

--- spring-sleuth-stream/sleuth-service-3/src/main/java/org/molecule/demo/sleuthservice3/MyController.java
+++ spring-sleuth-stream/sleuth-service-3/src/main/java/org/molecule/demo/sleuthservice3/MyController.java
@@ -3,7 +3,8 @@ package org.molecule.demo.sleuthservice3;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,17 +18,21 @@ public class MyController {
 
     private static final Logger logger = LoggerFactory.getLogger(MyController.class);
 
-    private final KafkaTemplate kafkaTemplate;
-
     @Autowired
-    public MyController(KafkaTemplate kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
+    private Source source;
+
+
+//    private final KafkaTemplate kafkaTemplate;
+
+//    @Autowired
+//    public MyController(KafkaTemplate kafkaTemplate) {
+//        this.kafkaTemplate = kafkaTemplate;
+//    }
 
     @RequestMapping("hi")
     public String sayHi() {
         logger.info("Say hi");
-        kafkaTemplate.send("vcredit", "Test Message".getBytes());
+        source.output().send(MessageBuilder.withPayload("aaa").build());
         return "hi";
     }
 }
--- spring-sleuth-stream/sleuth-service-3/src/main/java/org/molecule/demo/sleuthservice3/SleuthService3Application.java
+++ spring-sleuth-stream/sleuth-service-3/src/main/java/org/molecule/demo/sleuthservice3/SleuthService3Application.java
@@ -2,19 +2,17 @@ package org.molecule.demo.sleuthservice3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.sleuth.stream.SleuthSink;
-import org.springframework.cloud.sleuth.stream.SleuthStreamAutoConfiguration;
-import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.cloud.stream.messaging.Source;
 
-@EnableZipkinStreamServer
-@EnableBinding(SleuthSink.class)
-@SpringBootApplication(exclude = SleuthStreamAutoConfiguration.class)
-@MessageEndpoint
+//@EnableZipkinStreamServer
+//@EnableBinding(SleuthSink.class)
+@SpringBootApplication
+//@MessageEndpoint
+@EnableBinding(Source.class)
 public class SleuthService3Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SleuthService3Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SleuthService3Application.class, args);
+    }
 }
